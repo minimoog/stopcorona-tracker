e4df63bdabf217d76de19ce8681d0a509bbddcbd
Toni Jovanoski <minimoog77@gmail.com>
2020-04-13 17:26:05 +0200

    Working on it...

--- stopcorona-tracker/ViewController.swift
+++ stopcorona-tracker/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CBCentralManagerDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var cbManager: CBCentralManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        cbManager = CBCentralManager(delegate: self, queue: nil)
     }
-
-
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .poweredOff:
+            print("BLE powered off")
+        case .poweredOn:
+            print("BLE powered on")
+            cbManager?.scanForPeripherals(withServices: nil, options: nil) // UUID?????
+        case .resetting:
+            print("BLE resetting")
+        case .unauthorized:
+            print("BLE unauthorized")
+        case .unsupported:
+            print("BLE unsupported")
+        default:
+            return;
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        
+    }
 }
 
